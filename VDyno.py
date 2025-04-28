b9a0effa9d9e3ddbc1ebb09afbf4a09d3bf98216
Daniel M <danielmuir167@gmail.com>
2025-04-28 08:20:20 +0100

    Changed preamble

--- VDyno.py
+++ VDyno.py
@@ -6,7 +6,7 @@ This code is the entry point, initialising the model, view, and presenter compon
 ===========================================================
 If testing without CAN tranceiver, switch can_handler.py to dummy_can_handler.py in VDyno/model/dyno.py.
 If you are using different hardware, you will need to modify the model/value_calibration.csv file to suit your needs. Values go factor, offset.
-If using Apple or Linux device, 
+If using Apple or Linux device, comment out line 238 of VDyno/view/main_window.py: "ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)"
 ============================================================
 
 written by:
